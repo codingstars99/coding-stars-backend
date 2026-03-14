1d34f4f52ec6a53e6c51864e0914016e715eb277
codingstars99 <codingstars99@gmail.com>
2026-03-15 02:26:59 +0530

    Create __init__.py

---
+++ routes/__init__.py
@@ -0,0 +1,12 @@
+"""
+Routes package for Coding Stars API
+
+This package contains all API route modules:
+- enrollments: Handle student enrollment requests
+- courses: Manage course information
+- games: Showcase published games
+- testimonials: Display student testimonials
+- schedule: Show class schedules
+"""
+
+__all__ = ['enrollments', 'courses', 'games', 'testimonials', 'schedule']
